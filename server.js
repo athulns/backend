c5fb90512ef1008426c67bcca5d1902e6babb799
Athul Suresh N <88792672+athulns@users.noreply.github.com>
2025-03-11 20:06:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,19 +18,10 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/recipes", recipeRoutes);
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-
 // Global Error Handling
 app.use((err, req, res, next) => {
   console.error(" Server Error:", err.message);
   res.status(500).json({ message: "Server Error" });
-});
-
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
